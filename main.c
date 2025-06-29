d6c954404964e945a420e7507afa3c3887e7978f
seajee <teapods@proton.me>
2025-06-29 22:53:25 +0200

    Refactoring

--- main.c
+++ main.c
@@ -10,13 +10,15 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-#define WINDOW_SIZE 1000
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 400
+#define INITIAL_SCALE 2.0
 #define INITIAL_RESOLUTION 0.5
 #define INITIAL_ITERATIONS 100
 #define MANDEL_INFINITY 16.0
 #define SPEED 0.5
 
-#define OUTPUT_RESOLUTION 4000 // 16384
+#define OUTPUT_WIDTH 4000 // 16384
 #define OUTPUT_ITERATIONS 4000
 #define OUTPUT_PATH "output.png"
 
@@ -48,97 +50,117 @@ double clamp(double value, double min, double max)
     return result;
 }
 
-void render_to_image(Vector2Double camera, double scale)
+void render(Vector2Double camera, Vector2Double scale, double resolution, int iterations, bool realtime)
 {
-    printf("INFO: Starting rendering\n");
+    int width;
+    int height;
+    int pixel_width;
+    int pixel_height;
 
     struct timespec start, end;
-    clock_gettime(CLOCK_MONOTONIC, &start);
-
-    int width = OUTPUT_RESOLUTION;
-    int height = OUTPUT_RESOLUTION;
     int comp = 3;
-    int size = width * height * comp;
-
-    uint8_t *pixels = malloc(size * sizeof(*pixels));
-    assert(pixels != NULL);
-
+    uint8_t *pixels = NULL;
     int prev_percent = -1;
 
-    for (int y = 0; y < height; ++y) {
-        int percent = (double)y / height * 100.0;
-        if (percent != prev_percent) {
-            printf("%d%%\r", percent);
-            fflush(stdout);
-            prev_percent = percent;
+    if (realtime) {
+        width = GetScreenWidth();
+        height = GetScreenHeight();
+    } else {
+        printf("INFO: Starting rendering\n");
+        clock_gettime(CLOCK_MONOTONIC, &start);
+
+        double screen_ratio = (double)GetScreenHeight() / GetScreenWidth();
+        width = OUTPUT_WIDTH;
+        height = width * screen_ratio;
+
+        pixels = malloc(width * height * comp * sizeof(*pixels));
+        assert(pixels != NULL);
+    }
+
+    pixel_width = width / (width * resolution);
+    pixel_height = height / (height * resolution);
+
+    for (int y = 0; y < height; y += pixel_height) {
+        if (!realtime) {
+            int percent = (double)y / height * 100.0;
+            if (percent != prev_percent) {
+                printf("%d%%\r", percent);
+                fflush(stdout);
+                prev_percent = percent;
+            }
         }
 
-#pragma omp parallel for
-        for (int x = 0; x < width; ++x) {
-            double a = map(x, 0, width,  camera.x - scale, camera.x + scale);
-            double b = map(y, 0, height, camera.y - scale, camera.y + scale);
-            double ca = a;
-            double cb = b;
+        for (int x = 0; x < width; x += pixel_width) {
+            double z_real = map(x, 0, width,  camera.x - scale.x, camera.x + scale.x);
+            double z_imag = map(y, 0, height, camera.y - scale.y, camera.y + scale.y);
+            double c_real = z_real;
+            double c_imag = z_imag;
 
             int i;
-            for (i = 0; i < OUTPUT_ITERATIONS; ++i) {
-                double aa = a*a - b*b;
-                double bb = 2*a*b;
+            for (i = 0; i < iterations; ++i) {
+                double new_z_real = z_real*z_real - z_imag*z_imag;
+                double new_z_imag = 2*z_real*z_imag;
 
-                a = aa + ca;
-                b = bb + cb;
+                z_real = new_z_real + c_real;
+                z_imag = new_z_imag + c_imag;
 
-                if (fabs(a + b) > MANDEL_INFINITY) {
+                if (fabs(z_real + z_imag) > MANDEL_INFINITY) {
                     break;
                 }
             }
 
-            int bright = 0;
-            // Color color = BLACK;
-            if (i != OUTPUT_ITERATIONS) {
-                double norm = normalize(i, 0.0, OUTPUT_ITERATIONS);
-                // double n = (a + b) / MANDEL_INFINITY;
-                bright = sqrtf(norm) * 255;
-                // color = ColorFromHSV(sqrtf(n) * 360.0f, 1.0f, 1.0f);
+            Color color = BLACK;
+            if (i != iterations) {
+                double norm = normalize(i, 0.0, iterations);
+                double bright = sqrt(norm) * 255;
+                color = (Color){ bright, bright, bright, 255 };
             }
 
-            int pix = (x + y*width) * comp;
-            pixels[pix + 0] = bright;
-            pixels[pix + 1] = bright;
-            pixels[pix + 2] = bright;
+            if (realtime) {
+                DrawRectangle(x, y, pixel_width, pixel_height, color);
+            } else {
+                int pix = (x + y*width) * comp;
+                pixels[pix + 0] = color.r;
+                pixels[pix + 1] = color.g;
+                pixels[pix + 2] = color.b;
+            }
         }
     }
 
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    long delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec -
-            start.tv_nsec) / 1000;
-    long ms = delta_us / 1000;
-
-    printf("INFO: Rendering took %ldms\n", ms);
-    printf("INFO: Starting saving\n");
-
-    clock_gettime(CLOCK_MONOTONIC, &start);
-
-    int res = stbi_write_png(OUTPUT_PATH, width, height, comp, pixels, width * comp);
-    if (res == 0) {
-        fprintf(stderr, "ERROR: Could not render output image\n");
-    } else {
+    if (!realtime) {
         clock_gettime(CLOCK_MONOTONIC, &end);
-        delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec -
+        long delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec -
                 start.tv_nsec) / 1000;
-        ms = delta_us / 1000;
-        printf("INFO: Saving took %ldms\n", ms);
-    }
+        long ms = delta_us / 1000;
 
-    free(pixels);
+        printf("INFO: Rendering took %ldms\n", ms);
+        printf("INFO: Starting saving\n");
+
+        clock_gettime(CLOCK_MONOTONIC, &start);
+
+        int res = stbi_write_png(OUTPUT_PATH, width, height, comp, pixels, width * comp);
+        if (res == 0) {
+            fprintf(stderr, "ERROR: Could not render output image\n");
+        } else {
+            clock_gettime(CLOCK_MONOTONIC, &end);
+            delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec -
+                    start.tv_nsec) / 1000;
+            ms = delta_us / 1000;
+            printf("INFO: Saving took %ldms\n", ms);
+        }
+
+        free(pixels);
+    }
 }
 
 int main(void)
 {
-    InitWindow(WINDOW_SIZE, WINDOW_SIZE, "mandelbrot");
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_MSAA_4X_HINT);
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "mandelbrot");
 
+    double screen_ratio = (double)WINDOW_HEIGHT / WINDOW_WIDTH;
     Vector2Double camera = { 0.0, 0.0 };
-    double scale = 2.0;
+    Vector2Double scale = { INITIAL_SCALE, INITIAL_SCALE * screen_ratio };
     double resolution = INITIAL_RESOLUTION;
     int iterations = INITIAL_ITERATIONS;
 
@@ -146,28 +168,31 @@ int main(void)
         float dt = GetFrameTime();
         int width = GetScreenWidth();
         int height = GetScreenHeight();
-        int pixel_width = width / (width * resolution);
-        int pixel_height = height / (height * resolution);
+        screen_ratio = (double)height / width;
+        scale.y = scale.x * screen_ratio;
 
         if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-            scale -= SPEED * scale * dt;
+            scale.x -= SPEED * scale.x * dt;
+            scale.y -= SPEED * scale.y * dt;
         }
         if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON)) {
-            scale += SPEED * scale * dt;
+            scale.x += SPEED * scale.x * dt;
+            scale.y += SPEED * scale.y * dt;
         }
-        scale += -1.0 * GetMouseWheelMove() * SPEED * scale;
+        scale.x += -1.0 * GetMouseWheelMove() * SPEED * scale.x;
+        scale.y += -1.0 * GetMouseWheelMove() * SPEED * scale.y;
 
         if (IsKeyDown(KEY_W)) {
-            camera.y -= SPEED * scale * dt;
+            camera.y -= SPEED * scale.y * dt;
         }
         if (IsKeyDown(KEY_A)) {
-            camera.x -= SPEED * scale * dt;
+            camera.x -= SPEED * scale.x * dt;
         }
         if (IsKeyDown(KEY_S)) {
-            camera.y += SPEED * scale * dt;
+            camera.y += SPEED * scale.y * dt;
         }
         if (IsKeyDown(KEY_D)) {
-            camera.x += SPEED * scale * dt;
+            camera.x += SPEED * scale.x * dt;
         }
 
         if (IsKeyPressed(KEY_RIGHT_SHIFT)) {
@@ -187,46 +212,17 @@ int main(void)
         }
 
         if (IsKeyPressed(KEY_R)) {
-            render_to_image(camera, scale);
+            render(camera, scale, 1.0, OUTPUT_ITERATIONS, false);
         }
 
         BeginDrawing();
         ClearBackground(BLACK);
 
-        for (int y = 0; y < height; y += pixel_height) {
-            for (int x = 0; x < width; x += pixel_width) {
-                double a = map(x, 0.0, width,  camera.x - scale, camera.x + scale);
-                double b = map(y, 0.0, height, camera.y - scale, camera.y + scale);
-                double ca = a;
-                double cb = b;
-
-                int i;
-                for (i = 0; i < iterations; ++i) {
-                    double aa = a*a - b*b;
-                    double bb = 2*a*b;
-
-                    a = aa + ca;
-                    b = bb + cb;
-
-                    if (fabs(a + b) > MANDEL_INFINITY) {
-                        break;
-                    }
-                }
-
-                Color color = BLACK;
-                if (i != iterations) {
-                    double norm = normalize(i, 0.0, iterations);
-                    double bright = sqrt(norm) * 255;
-                    color = (Color){ bright, bright, bright, 255 };
-                }
-
-                DrawRectangle(x, y, pixel_width, pixel_height, color);
-            }
-        }
+        render(camera, scale, resolution, iterations, true);
 
         DrawText(TextFormat("Iterations: %i", iterations), 10, 10, 20, GREEN);
         DrawText(TextFormat("Resolution: %f", resolution), 10, 30, 20, GREEN);
-        DrawText(TextFormat("Scale: %f", scale), 10, 50, 20, GREEN);
+        DrawText(TextFormat("Scale: (%f, %f)", scale.x, scale.y), 10, 50, 20, GREEN);
         DrawText(TextFormat("Camera: (%f, %f)", camera.x, -camera.y), 10, 70, 20, GREEN);
 
         EndDrawing();
